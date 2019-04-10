8675a5f83de433b86f463586ec866341419a840e
Dinh Van Hoa <hoadv9@gmail.com>
2019-04-10 16:35:30 +0700

    update Danh mục khung giờ khám hoadv

---
+++ Clients/SHC Admin/ClientApp/src/app/booking-informations/booking-informations.module.ts
@@ -0,0 +1,28 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { CommonModule } from '@angular/common';
+import { IndexComponent } from './index/index.component';
+import { BookingInformationsComponent } from './booking-informations.component';
+import { BookingInformationsRoutingModule } from './booking-informations-routing.module';
+import { NgModule } from '@angular/core';
+import { SharedModule } from '@shared/shared.module';
+import { TaskComponent } from './task/task.component';
+import { UtilsModule } from '@shared/utils/utils.module';
+import { ValidationModule } from '@app/validation/validation.module';
+import { NgxMaskModule} from 'ngx-mask';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule,
+    UtilsModule,
+    BookingInformationsRoutingModule,
+    ValidationModule,
+    NgxMaskModule.forRoot()
+  ],
+  declarations: [BookingInformationsComponent, IndexComponent, TaskComponent],
+  entryComponents: [TaskComponent]
+})
+export class BookingTimeslotsModule { }
