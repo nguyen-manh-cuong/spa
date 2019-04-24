5279ee2b603eabaa53b74bd95bbc5bc1ad8dedc2
Tung Duy <tungdd94tq@gmail.com>
2019-04-24 09:07:26 +0700

    Tungdd fix uat 20190423

--- Clients/SHC Admin/ClientApp/src/app/sms-package/task/task.component.ts
+++ Clients/SHC Admin/ClientApp/src/app/sms-package/task/task.component.ts
@@ -31,6 +31,8 @@ export class TaskComponent extends AppComponentBase implements OnInit, AfterView
     _details: Array<IPackageDetail> = [];
 
     @ViewChild("txtName") txtName: MatInput;
+    @ViewChild("smsTo") smsTo;
+    @ViewChild("detailCost") detailCost;
 
     constructor(
         injector: Injector, 
@@ -163,7 +165,7 @@ export class TaskComponent extends AppComponentBase implements OnInit, AfterView
         var value = this.getNumber(values);
 
         if (type == 1) {
-            var valueSmsTo = Number(this._frm.controls['smsTo'].value);
+            var valueSmsTo = Number(this.smsTo.nativeElement.value);
             var length = this._details.length;
             var totalQuantity = !length ? valueSmsTo : (length ? this._frm.controls['quantity'].value : valueSmsTo);
             index > -1 ? this._details[index].smsTo = value : "";
@@ -189,7 +191,8 @@ export class TaskComponent extends AppComponentBase implements OnInit, AfterView
                 }
             }
         } else {
-            var totalCost: number = type == 1 ? Number(this._frm.controls['detailCost'].value) + Number(value) : Number(this._frm.controls['detailCost'].value);
+            var totalCost: number = type == 1 ? Number(this.detailCost.nativeElement.value) + Number(value) : Number(this.detailCost.nativeElement.value);
+
             index > -1 ? this._details[index].cost = value : "";
 
             this._details.forEach(el => {
