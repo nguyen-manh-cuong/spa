c70d2537f1a86b6e6ef13ba791f04b34eeb9d026
toan <toannd@dft.vn>
2019-05-22 09:05:39 +0700

    merge

--- Servers/SHCServer/Models/SmsPackageDistribute.cs
+++ Servers/SHCServer/Models/SmsPackageDistribute.cs
@@ -11,12 +11,6 @@ namespace SHCServer.Models
         public int Id { set; get; }
         public int SmsPackageId { set; get; }
         public int HealthFacilitiesId { set; get; }
-        public string HealthFacilitiesName { set; get; }
-        public string HealthFacilitiesCode { set; get; }
-        public string SmsBrandsName { set; get; }
-        public string Name { set; get; }
-        public int Quantity { set; get; }
-        public long Cost { set; get; }
 
         public int SmsBrandsId { set; get; }
         public int YearStart { set; get; }
