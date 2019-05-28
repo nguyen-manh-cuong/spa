e7b307f90aecb192c5d8c1a14029d3aad339daff
Nguyen Van Quang <39783340+ahhungknow@users.noreply.github.com>
2019-05-28 15:42:42 +0700

    QuangNV hot fix

--- Clients/SHC Admin/ClientApp/src/auth/reset-password/reset-password.component.ts
+++ Clients/SHC Admin/ClientApp/src/auth/reset-password/reset-password.component.ts
@@ -4,7 +4,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { AbpSessionService } from '@abp/session/abp-session.service';
 import { AppComponentBase } from '@shared/app-component-base';
-import { LoginService } from './login.service';
+
 import { Router } from '@angular/router';
 import { IHealthfacilities } from '@shared/Interfaces';
 import { debounceTime, tap, switchMap, finalize } from 'rxjs/operators';
@@ -12,77 +12,17 @@ import { DataService } from '@shared/service-proxies/service-data';
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss'],
+    selector: 'app-reset-password',
+    templateUrl: './reset-password.component.html',
+    styleUrls: ['./reset-password.component.scss'],
     encapsulation: ViewEncapsulation.None
 })
-export class LoginComponent extends AppComponentBase implements OnInit {
+export class ResetComponent extends AppComponentBase implements OnInit {;
 
-    frmLogin: FormGroup;
-    submitted = false;
-    saving = true;
-    isLoading = false;
-    dataService: DataService;
-    numberLoginFail = 0;
-
-    constructor(private _sanitizer: DomSanitizer, private _dataService: DataService, private http: HttpClient, injector: Injector, public loginService: LoginService, private _formBuilder: FormBuilder, private _router: Router, private _sessionService: AbpSessionService) {
+    constructor(private _sanitizer: DomSanitizer, private _dataService: DataService, private http: HttpClient, injector: Injector, private _formBuilder: FormBuilder, private _router: Router, private _sessionService: AbpSessionService) {
         super(injector);
     }
 
     ngOnInit(): void {
-        this.frmLogin = this._formBuilder.group({
-            userNameOrEmailAddress: [this.loginService.authenticateModel.userNameOrEmailAddress, Validators.required],
-            password: [this.loginService.authenticateModel.password, Validators.required],
-            codeCapcha: [''],
-        });
-        this.dataService = this._dataService;
-    }
-
-    get f() { return this.frmLogin.controls; }
-    get multiTenancySideIsTeanant(): boolean {
-        return this._sessionService.tenantId > 0;
-    }
-    get isSelfRegistrationAllowed(): boolean {
-        if (!this._sessionService.tenantId) {
-            return false;
-        }
-
-        return true;
-    }
-
-    capcha = false;
-    _capcha: { code: string, data: any } = { code: '', data: '' };
-
-
-    login(): void {
-        let numLoginFail = 0;
-        if (localStorage.getItem('logCount') != null) {
-            numLoginFail = parseInt(localStorage.getItem('logCount'));
-        }
-
-        if (numLoginFail > 5) {
-            if (this.frmLogin.controls['codeCapcha'].value != this._capcha.code) {
-                this.capcha = true;
-                return;
-            }
-        }
-
-        this.submitted = true;
-        if (this.frmLogin.invalid) { return; }
-        // this.submitting = true;
-
-        this.loginService.authenticateModel = Object.assign(this.loginService.authenticateModel, this.frmLogin.value);
-        this.loginService.authenticate(() => { }, error => {
-            localStorage.setItem('logCount', (numLoginFail + 1).toString())
-        });
-    }
-
-    getCapcha() {
-        this._dataService.getAny('get-captcha-image').subscribe(res => this._capcha = { code: res.code, data: this._sanitizer.bypassSecurityTrustResourceUrl('data:image/jpg;base64,' + res.data) });
-    }
-
-    validateCapcha(value: any) {
-        if (value.length == 4) this._capcha.code != value ? this.capcha = true : this.capcha = false;
     }
 }
