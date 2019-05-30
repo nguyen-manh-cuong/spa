e4d2f68abd54f2420d6681c8c6f5ff76057cd859
Dinh Van Hoa <hoadv9@gmail.com>
2019-05-30 17:52:14 +0700

    update: UserLoginViewModel: hoadv

---
+++ Servers/SHCServer/ViewModels/UserLoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SHCServer.ViewModels
+{
+    public class UserLoginViewModel
+    {
+        public int? Id { get; set; }
+        public int? Counter { get; set; }
+        public DateTime? LockedTime { get; set; }
+        public int? Status { get; set; }
+        public DateTime? ExpriredDate { get; set; }
+        public int? MdmStatus { get; set; }
+    }
+}
