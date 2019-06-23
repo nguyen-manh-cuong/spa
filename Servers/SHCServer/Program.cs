71143e835cded5e8e7389d6808b5aeb1b520fe90
Nguyễn Mạnh Cường <cuongnm>
2019-06-23 15:35:03 +0700

    Fix JIRA : 995

--- Servers/SHCServer/Program.cs
+++ Servers/SHCServer/Program.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Viettel.Infrastructure;
 using Viettel.Infrastructure.Interception;
-using System.Data;
-using SHCServer.Models;
 
 namespace SHCServer
 {
@@ -14,19 +11,15 @@ namespace SHCServer
     {
         public static void Main(string[] args)
         {
-            //SendMail.Start();
             IDbCommandInterceptor interceptor = new DbCommandInterceptor();
             DbConfiguration.UseInterceptors(interceptor);
 
             RegisterMappingType();
             BuildWebHost(args).Run();
-            
         }
 
         public static IWebHost BuildWebHost(string[] args)
         {
-            
-
             return WebHost
                    .CreateDefaultBuilder(args)
                    .ConfigureAppConfiguration(config =>
@@ -34,17 +27,19 @@ namespace SHCServer
                        config.AddJsonFile("appsettings.json");
                        config.AddEnvironmentVariables();
                    })
+                   .UseIISIntegration()
+                   .UseKestrel(c => c.AddServerHeader = false)
                    .UseStartup<Startup>()
                    .UseUrls("http://127.0.0.1:9008")
                    .Build();
         }
 
-        static void RegisterMappingType()
+        private static void RegisterMappingType()
         {
             DbConfiguration.UseMappingType(new StringMappingType());
             DbConfiguration.UseMappingType(new MappingType<int>(DbType.Int32));
             DbConfiguration.UseMappingType(new MappingType<long>(DbType.Int64));
-            DbConfiguration.UseMappingType(new MappingType<Int16>(DbType.Int16));
+            DbConfiguration.UseMappingType(new MappingType<short>(DbType.Int16));
             DbConfiguration.UseMappingType(new MappingType<byte>(DbType.Byte));
             DbConfiguration.UseMappingType(new MappingType<double>(DbType.Double));
             DbConfiguration.UseMappingType(new MappingType<float>(DbType.Single));
