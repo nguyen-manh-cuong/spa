65a59f9bd08699ba893732d1aa725aa38a29823f
Lam Doan <lamlemlinh1997@gmail.com>
2019-06-26 11:02:28 +0700

    fix UAT 7, 11 excel

--- Clients/SHC Admin/ClientApp/src/app/booking-doctor-approve/index/index.component.ts
+++ Clients/SHC Admin/ClientApp/src/app/booking-doctor-approve/index/index.component.ts
@@ -226,7 +226,10 @@ export class IndexComponent extends PagedListingComponentBase<IBookingDoctorsCal
 
     onSelectHealthFacilities(obj: any) {
         this._doctors = [];
-        this.dataService.getAll('doctors', obj.healthFacilitiesId).subscribe(resp => this._doctors = resp.items);
+        this.dataService.getAll('doctors', obj.healthFacilitiesId).subscribe(resp => {
+            this._doctors = resp.items;
+            console.log(resp.items)
+        });
     }
 
     //summit
