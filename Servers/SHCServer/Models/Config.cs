c3b80712a9eff7f4eb334532bedb53f0592d2b4b
hoangnv <hoangnv@dft.vn>
2019-06-29 10:29:52 +0700

    fix tạo tài khoản 84, check theo db sys_configs

--- Servers/SHCServer/Models/Config.cs
+++ Servers/SHCServer/Models/Config.cs
@@ -18,5 +18,14 @@ namespace SHCServer.Models
         public int Id { set; get; }
         public string Code { set; get; }
         public string Values { set; get; }
+        public string Description { get; set; }
+
+        //audi
+        public bool IsDelete { get; set; }
+        public bool? IsActive { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public int? CreateUserId { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public int? UpdateUserId { get; set; }
     }
 }
